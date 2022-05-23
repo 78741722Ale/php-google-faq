6b2c37415a736fc77345fff7292c9ada9f959873
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-23 21:17:15 +0200

    Import di bootstrap (tramite cdn), creato foglio di stile con altezze ai container

--- index.php
+++ index.php
@@ -11,18 +11,33 @@ Gestire il “Database” e la visualizzazione di queste domande e risposte con 
 $database = [
     "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?" =>
         [
-        "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.",
-        "Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.",
-        "Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. 
-        Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.",
-        "Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.",
-        "Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione."                
+        "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. 
+        La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. 
+        Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.",
+        "Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. 
+        Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali 
+        ed il diritto di tutti di conoscere e distribuire le informazioni.",
+        "Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. 
+        Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). 
+        Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. 
+        Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, 
+        negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, 
+        in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, 
+        puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.",
+        "Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. 
+        La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia 
+        importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.",
+        "Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto 
+        delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione."                
         ],
     "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?" =>
         [
-        "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.",
-        "Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. 
-        Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.",
+        "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti 
+        che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.",
+        "Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. 
+        Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. 
+        Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. 
+        Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.",
         "Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.",
         "Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo."                  
         ],
@@ -49,9 +64,8 @@ $database = [
         ],    
 ];
 
-var_dump($database);
+/* var_dump($database); */
 /* Avvio il forEach per estrapolare ogni singolo Array interno */
-
 ?>
 
 <!DOCTYPE html>
@@ -61,13 +75,32 @@ var_dump($database);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP google Faq </title>
+    <!-- Link Foglio stile Bootstrap -->
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>
+    <!-- Link a foglio di style -->
+    <link rel='stylesheet' href='./assets/css/style.css'>
 </head>
 <body>
+    
+    <!-- /#site_header -->
+    <div id="site_header">
+
+
+    </div>
+    <!-- /#site_main -->    
+    <div id="site_main">
+    <!-- Tutto questo racchiuso in un container md/xl -->
+
     <!-- Primo For Each per la struttura dati -->
     <!-- $database => array di riferimento -->
     <!-- $faqs(date) => indici array di riferimento, è la key -->
     <!-- $faq(posts) => indici array di riferimento, è la key -->
-    <?php foreach ($database as $faqs => $faq) : ?>
+    <?php foreach ($database as $faqs => $faq) : 
+        /* var_dump("Questa è la Key (faqs) ==> in pratica il titolo al di fuori dell'array");
+        var_dump($faqs);
+        var_dump("Questo è la Value (faq) ==> in pratica il contenuto (stringhe) dei vari array");
+        var_dump($faq); */
+        ?>
         <h2> <?= $faqs?></h2>
         <!-- $faq(posts) => è la key -->
         <!-- $singleFaq($post) => è il value -->
@@ -76,6 +109,16 @@ var_dump($database);
         <!-- Closure del secondo for each -->    
         <?php endforeach; ?> 
         <!-- Closure del primo for each -->
-    <?php endforeach; ?>        
+    <?php endforeach; ?> 
+
+    </div>
+    
+
+
+
+    
+    
+    <!-- Link a JS Bootstrap -->
+    <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js' integrity='sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM' crossorigin='anonymous'></script>
 </body>
 </html>
