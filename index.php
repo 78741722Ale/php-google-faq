4338bf705165c902879cb655dbdabc59647b5eea
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-23 14:33:40 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+
+/* Consegna esercizio
+Riscrivere questa pagina del sito google https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+*/
+
+
+/* Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP */
+$database = [
+
+
+
+];
+
+
+var_dump("Questa è la verifica degli argomenti presenti nel database");
+var_dump($database);
+
+
+
+?>
